ce9261780da845282130075087a8d709a5a5ebb7
Avonakub <avonakub@icloud.com>
2026-04-09 01:36:01 +0300

    добавлена таблица действий пользователя

--- db_telegrambot.py
+++ db_telegrambot.py
@@ -1,4 +1,5 @@
 import psycopg2
+import datetime
 
 
 def connect_db():
@@ -17,9 +18,20 @@ def initialize_db(conn):
         query = """CREATE TABLE IF NOT EXISTS users (
             id SERIAL PRIMARY KEY,
             username VARCHAR(100) NOT NULL,
-            telegram_id BIGINT UNIQUE NOT NULL
-        )
+            telegram_id BIGINT UNIQUE NOT NULL,
+            registered TIMESTAMP NOT NULL DEFAULT NOW()
+        );
+        
+        CREATE TABLE IF NOT EXISTS user_actions (
+            id SERIAL PRIMARY KEY,
+            telegram_id BIGINT REFERENCES users(telegram_id),
+            action_user TEXT NOT NULL,
+            user_message TEXT,
+            bot_response TEXT,
+            created_at TIMESTAMP DEFAULT NOW()
+        );
         """
+
         cur.execute(query)
         conn.commit()
 
@@ -27,12 +39,12 @@ def initialize_db(conn):
 def register_user(conn,username=None, telegram_id=None):
     with conn.cursor() as cur:
         cur.execute("""
-            INSERT INTO users (username, telegram_id) 
+            INSERT INTO users (username, telegram_id, registered) 
             VALUES (%s, %s)
             ON CONFLICT (telegram_id) DO NOTHING
         """, (username, telegram_id))
         conn.commit()
-        print(f"User {username} with ID {telegram_id} registered!")
+        print(f"User {username} with ID {telegram_id} registered at {datetime.datetime.now()}!")
 
 
 def main():
